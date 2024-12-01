409b55da19fd06deddb67788f94d3bd968309ff7
Altin Pajaziti <altin-pajaziti@outlook.com>
2024-12-01 13:20:59 +0100

    ...ordersUpdate

--- src/app/core/Services/orders.service.ts
+++ src/app/core/Services/orders.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
+import { environment } from 'src/environments/envirement';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdersService {
+  private apiUrl = environment.api_Url + 'PurchasedProducts';
 
-  constructor() { }
+  constructor(private http: HttpClient, public auth: AuthenticationService) {}
+
+  getAllOrders() {
+    return this.http.get(this.apiUrl + '/Get-all-product-that-arebought');
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete(`${this.apiUrl}/Delete-Product/${id}`);
+  }
 }
